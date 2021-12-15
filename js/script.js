c76432b43f08b84c3eafd50696e1224b8af7dd0f
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-12-15 14:24:52 +0100

    add h1 via vue

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@ Descrizione:
 Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */
+
+//utilizzo Vue JS
+const app = new Vue (
+    {
+        el: '#datas', //prendo l'id dell'elemento su cui voglio lavorare
+        data: { //inserisco i dati
+            message: 'Questo è un messaggio di test in un h1',
+        }
+    }
+);
