a0e07455a9498535341e366bcfdecb6ff96dd5e3
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-12-15 14:44:38 +0100

    add methods and image2

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -19,7 +19,19 @@ const app = new Vue (
             image: 'https://www.aiapp.net/wp-content/uploads/2020/09/Agricoltura.jpg',
             small: 'small-img',
             text: '',
-            placeholder: 'Inserisci testo'
+            placeholder: 'Inserisci testo',
+            image2: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp-spacegray-select-202011_GEO_IT?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1632948900000',
+            medium: 'medium-img',
+            m1: 'm-1',
+            num: 3
+        },
+        methods: {
+            addOneProduct: function () {
+                this.num += 1;
+            },
+            rmvOneProduct: function (){
+                this.num -=1;
+            }
         }
     }
 );
